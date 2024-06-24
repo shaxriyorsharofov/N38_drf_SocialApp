ca99e1801d3c498d7391e1a1987f1e8b9dbecd28
shaxriyorsharofov <shaxriyorsharofov911@gmail.com>
2024-06-24 17:44:26 +0500

    post done

--- shared/models.py
+++ shared/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4(), unique=True, editable=False, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4(), editable=False, primary_key=True)
 
     class Meta:
         abstract = True
--- social/urls.py
+++ social/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('user/', include('users.urls'))
+    path('user/', include('users.urls')),
+    path('post/', include('post.urls'))
 ]
 
 
